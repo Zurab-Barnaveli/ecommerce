da64da3c0f091658de4ede38f5ab518c901d232e
Zurab-Barnaveli <zuka.barnaveli11@gmail.com>
2023-06-20 01:02:53 +0400

    api.more

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -36,4 +36,43 @@ export const registerUser = async (
   });
 };
 
-// ... Implement other API calls (addProduct, changeProduct, deleteUser, loginUser, getUserInfo, changeUserInfo) ...
+export const loginUser = async (email: string, password: string) => {
+  return axios.post(`${BASE_URL}/login`, {
+    email,
+    password,
+  });
+};
+
+export const addProduct = async (token: string, productData: any) => {
+  return axios.post(`${BASE_URL}/product`, productData, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const changeProduct = async (
+  token: string,
+  productId: number,
+  productData: any
+) => {
+  return axios.put(`${BASE_URL}/product/${productId}`, productData, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const deleteProduct = async (token: string, productId: number) => {
+  return axios.delete(`${BASE_URL}/product/${productId}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const getUserInfo = async (token: string) => {
+  return axios.get(`${BASE_URL}/me`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export const changeUserInfo = async (token: string, userData: any) => {
+  return axios.post(`${BASE_URL}/user`, userData, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
